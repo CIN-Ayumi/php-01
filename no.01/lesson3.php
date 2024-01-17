583c811b4d4bbedf531063c22b5420fe2d75d446
CIN-Ayumi <155986196+CIN-Ayumi@users.noreply.github.com>
2024-01-18 01:43:31 +0900

    Add files via upload

---
+++ no.01/lesson3.php
@@ -0,0 +1,14 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+$personalData = [ name => '山田', age => '20', gender => '女性'];
+
+// 山田
+// 20歳・女性
+// と出力してください。
+echo '<p>', $personalData[name], '</p>';
+echo '<p>', $personalData[age], '・', $personalData[gender], '</p>';
+?>
+
