f28e5d3257e7817241e601b8dd6643b529208ab3
CIN-Ayumi <155986196+CIN-Ayumi@users.noreply.github.com>
2024-01-19 04:12:29 +0900

    Update lesson3.php

--- no.01/lesson3.php
+++ no.01/lesson3.php
@@ -3,12 +3,12 @@
 // name, age, genderに
 // 山田,  20,  女性
 // という値を格納し、
-$personalData = [ name => '山田', age => '20', gender => '女性'];
+$personalData = [ 'name' => '山田', 'age' => '20', 'gender' => '女性'];
 
 // 山田
 // 20歳・女性
 // と出力してください。
-echo '<p>', $personalData[name], '</p>';
-echo '<p>', $personalData[age], '・', $personalData[gender], '</p>';
+echo '<p>', $personalData['name'], '</p>';
+echo '<p>', $personalData['age'], '・', $personalData['gender'], '</p>';
 ?>
 
